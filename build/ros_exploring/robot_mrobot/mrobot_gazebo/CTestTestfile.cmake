e8d5c7014d01ae6e494ef798fdfa5edf14376d66
coco721 <80469042@qq.com>
2020-03-27 14:35:02 +0800

    delet

--- build/ros_exploring/robot_mrobot/mrobot_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/guo/git/src/ros_exploring/robot_mrobot/mrobot_gazebo
-# Build directory: /home/guo/git/build/ros_exploring/robot_mrobot/mrobot_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
