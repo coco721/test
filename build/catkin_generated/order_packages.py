76234c12783f5057bbd9a6ae05bc9a8437c16868
coco721 <80469042@qq.com>
2020-03-27 14:39:31 +0800

    d

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/guo/git/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/guo/autoware.ai/devel;/home/guo/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/guo/autoware.ai/devel;/home/guo/catkin_ws/devel;/opt/ros/kinetic" != "" else []
