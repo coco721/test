eecc3edb5e6e13570019ac6b1c695e8b2d202706
coco721 <80469042@qq.com>
2020-03-27 12:25:49 +0800

    2

---
+++ build/ros_exploring/robot_mrobot/mrobot_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/guo/git/src/ros_exploring/robot_mrobot/mrobot_bringup
+# Build directory: /home/guo/git/build/ros_exploring/robot_mrobot/mrobot_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
